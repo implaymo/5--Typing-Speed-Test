103646778741c80d001724976bd819656c2c06aa
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-25 21:20:42 +0000

    Add highscore label and timer label in order to show the timer and highscore.

--- gui.py
+++ gui.py
@@ -14,16 +14,22 @@ class Gui():
     title_label.grid(column=1, row=0)
 
     words_label = tk.Label(text="Words: ")
-    words_label.grid(column=0, row=1)
+    words_label.grid(column=0, row=2)
     
     
     words_text = tk.Label(text=f"{words.all_words_printed()}")
-    words_text.grid(column=1, row=1)
+    words_text.grid(column=1, row=2)
 
     words_entry = tk.Entry(width=100)
-    words_entry.grid(column=1, row=2)
+    words_entry.grid(column=1, row=3)
+    
+    highscore = tk.Label(text="HIGHSCORE: ")
+    highscore.grid(column=0,row=0)
 
-
+    timer = tk.Label(text="TIMER: ")
+    timer.grid(column=0, row=1)
+    
+    
     start_button = tk.Button(text="Start Test", width=10)
     start_button.grid(column=1, row=4)
     
